4c0b90f37c593aa8b35cdc1842daf9bd2d11b2a9
gracewilcox <graceawilcox@gmail.com>
2024-02-20 11:18:57 -0800

    move

---
+++ sdk/tscore/runtime/errors.go
@@ -0,0 +1,19 @@
+//go:build go1.18
+// +build go1.18
+
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+package runtime
+
+import (
+	"net/http"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/tscore/internal/exported"
+)
+
+// NewResponseError creates an *tscore.ResponseError from the provided HTTP response.
+// Call this when a service request returns a non-successful status code.
+func NewResponseError(resp *http.Response) error {
+	return exported.NewResponseError(resp)
+}
